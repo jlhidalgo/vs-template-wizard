99a80cd2767f6b54a2f6866a0a3a68224b538da2
Juan L. Hidalgo <juanluis.hidalgo@gmail.com>
2016-10-08 14:04:43 -0700

    Add WizardImplementation class

---
+++ VSTemplateWizard/VSTemplateWizard/WizardImplementation.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using EnvDTE;
+using System.Windows.Forms;
+
+namespace VSTemplateWizard
+{
+    public class WizardImplementation : IWizard
+    {
+        private UserInputForm _inputForm;
+        private string _customMessage;
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                // Display a form to the user. The form collects 
+                // input for the custom message.
+                _inputForm = new UserInputForm();
+                _inputForm.ShowDialog();
+
+                _customMessage = UserInputForm.CustomMessage;
+
+                // Add custom parameters.
+                replacementsDictionary.Add("$custommessage$", _customMessage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
