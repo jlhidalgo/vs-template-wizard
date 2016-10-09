5a79bcbf69e7ed24536b4c7679452501c1206073
Juan L. Hidalgo <juanluis.hidalgo@gmail.com>
2016-10-08 19:05:35 -0700

    Replace C# template by a SQL template

---
+++ VSTemplateWizard/SProcTemplate/Code.sql
@@ -0,0 +1,38 @@
+﻿/*
+    Database            Object Name											Object Type					Rollback Required?			Action			Bug/Story
+    ----------------    ------------------------------------				-----------------------		------------------------	------------	------------------------
+    $databasename$      $sprocname$											Stored Procedure			$rollbackrequired$			$action$		$itemnumber$
+*/
+USE [$databasename$]
+GO
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+
+PRINT 'Compile stored procedure [dbo].[$sprocname$]'
+GO
+
+IF OBJECT_ID('[dbo].[$sprocname$]') IS NULL
+    EXEC ('CREATE PROCEDURE [dbo].[$sprocname$] AS RAISERROR(N''Not Implemented'', 16, 1);');
+GO
+/***************************************************************************************************************************************************************
+*   dbo.$sprocname$
+*
+*    Purpose: ??
+*
+*    Author: ??
+*
+*    Origination Date: ??
+*
+*    History:
+*    Revision		By				Date			US/Bug			Task			Change
+*	 $revision$		$username$		$date$			$itemnumber$	$tasknumber$	$changecomments$
+*
+**************************************************************************************************************************************************************/
+ALTER PROCEDURE [dbo].[$sprocname$]     
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
