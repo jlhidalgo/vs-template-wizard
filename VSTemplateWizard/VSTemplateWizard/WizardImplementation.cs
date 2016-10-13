9206e2c1c26f894cb2fe78889be6571127493dd9
Juan L. Hidalgo <juanluis.hidalgo@gmail.com>
2016-10-12 21:21:18 -0700

    Add Cancel functionality

--- VSTemplateWizard/VSTemplateWizard/WizardImplementation.cs
+++ VSTemplateWizard/VSTemplateWizard/WizardImplementation.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using EnvDTE;
 using System.Windows.Forms;
+using System.IO;
 
 namespace VSTemplateWizard
 {
@@ -26,14 +27,16 @@ namespace VSTemplateWizard
         {
         }
 
-        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind, object[] customParams)
         {
             try
             {
                 // Display a form to the user. The form collects 
                 // input for the custom message.
                 _inputForm = new SprocInputForm();
-                _inputForm.ShowDialog();
+                if (_inputForm.ShowDialog() != DialogResult.OK)
+                    throw new WizardCancelledException();
 
                 // Add custom parameters.
                 replacementsDictionary.Add("$databasename$", SprocInputForm.DatabaseName);
@@ -46,7 +49,17 @@ namespace VSTemplateWizard
                 replacementsDictionary.Add("$date$", SprocInputForm.Date);
                 replacementsDictionary.Add("$tasknumber$", SprocInputForm.Task);
                 replacementsDictionary.Add("$changecomments$", SprocInputForm.Comments);
+            }
+            catch (WizardCancelledException)
+            {
+                var path = string.Format("{0}{1}\\{2}", replacementsDictionary["$solutiondirectory$"],
+                    replacementsDictionary["$rootnamespace$"], replacementsDictionary["$rootname$"]);
 
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                throw;
             }
             catch (Exception ex)
             {
